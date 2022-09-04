18c34c8fd9bd5d87c5bc312b4b9218df28361f24
Allanifba <110695866+Allanifba@users.noreply.github.com>
2022-09-04 19:26:33 -0300

    Add files via upload

---
+++ interpolacao.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+while True:
+    print('-----------------------------------  DESCRIÇÃO  ------------------------------------')
+    print('Programa que encontra o polinômio interpolador passando pelos pontos de abscissas\n'
+          '(x1,y1), (x2,y2), ..., (xn,yn).')
+    print('-----------------------------------  INSTRUÇÕES  -----------------------------------')
+    print('[1] Insira os valores das abscissas e das ordenadas separados por vírgula. \n'
+          '[2] A resposta será dada por um vetor linha contendo os coefincientes do polinômio\n'
+          'do maior para o menor grau.')
+    print('-----------------------------  ENTRE COM OS DADOS  ---------------------------------')
+
+    x = np.array(eval(input('Insira as abscisa: ')))
+    y = np.array(eval(input('Insira ordenadas: ')))
+    a = float(input('Digite o valor x no qual quer avaliar P(x): '))
+    pol = np.polyfit(x, y, np.size(x) - 1)  # polynomial coefficients
+
+    xx = np.linspace(min(x), max(x))
+    yy = np.polyval(pol, xx)  # polynomial value in the points contained in xx
+    plt.plot(xx, yy, '-', x, y, 'ro')
+    print('\n ***********************************  RESPOSTA  ***********************************\n')
+    print('Os coeficientes do polinômio são: ', pol)
+    print('O valor do polinômio em x = %d é: %0.6f' % (a, np.polyval(pol, a)))
+    plt.grid()  # Exibe a grade
+    plt.title('Polinômio Interpolador')
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.show()  # Exibe o gráfico
+    print('\n **********************************************************************************\n')
+    print('                       Autoria: Allan de Sousa Soares - IFBA VDC                       ')
+    print('             Canal: https://www.youtube.com/c/MatematicaParaGenteGrande\n              ')
+    esc = input('Digite Enter para continuar: ')
